276a0fb82b25598dd746662b50112847dd6f6a63
mcberros <mcberros@gmail.com>
2013-09-01 22:30:26 +0200

    Refactoring for Meaningful Names

--- translate_words.rb
+++ translate_words.rb
@@ -2,42 +2,47 @@ require 'watir-webdriver'
 require 'csv'
 require_relative 'word'
 
-Selenium::WebDriver::Firefox::Binary.path='/Applications/Firefox.app/Contents/MacOS/firefox'
+Selenium::WebDriver::Firefox::Binary.path ='/Applications/Firefox.app/Contents/MacOS/firefox'
+WORD_LATIN_AMERICA = 'LatAm'
+WRITE_ACCESS_FILE_MODE = 'wb'
 
-def obtainWordToSearch(rowCSV)
-  if not rowCSV[0].nil? and not rowCSV[1].nil?
-    arrayFirstColumn = rowCSV[0].split(',')
-    if arrayFirstColumn.length >= 1
-      term=Word.new()
-      term.wordNotSimplified=rowCSV[0]
-      term.sentences=rowCSV[1]
-      # Case:
-      word=arrayFirstColumn[0].strip
+def transformLineFileIntoWord(rowCSV)
+  wordNotSimplified = rowCSV[0] if not rowCSV[0].nil?
+  wordExamples = rowCSV[1] if not rowCSV[1].nil?
+
+  if wordNotSimplified and wordExamples
+    arrayWordNotSimplified = wordNotSimplified.split(',')
+
+    if arrayWordNotSimplified.length >= 1
+      word = Word.new
+      word.wordNotSimplified = wordNotSimplified
+      word.sentences = wordExamples
+      cleanWord = arrayWordNotSimplified[0].strip
       
-      if arrayFirstColumn.length > 1
-        # Verb without sich. Example: "abbiegen, biegt ab, bog ab, ist abgebogen"
-        term.type=Word::TYPE[:verb]
-      elsif arrayFirstColumn.length == 1
+      if arrayWordNotSimplified.length > 1
+        if cleanWord.match('^(der|die|das) ')
+          # Case: Noun. Example: "das Abonnement, -s"
+          cleanWord = arrayWordNotSimplified[0].split[1].strip
+          word.type = Word::TYPE[:noun]
+        elsif cleanWord.match(' sich$')
+          # Verb with sich. Example: Example: "amüsieren sich, amüsiert sich, amüsierte sich, hat sich amüsiert"
+          cleanWord = cleanWord.sub(' sich$','')
+          word.type = Word::TYPE[:verb]
+        else  
+          # Verb without sich. Example: "abbiegen, biegt ab, bog ab, ist abgebogen"
+          word.type = Word::TYPE[:verb]
+        end
+      elsif arrayWordNotSimplified.length == 1
         # Other word different from noun or verb: ab
-        term.type=Word::TYPE[:other]
+        word.type = Word::TYPE[:other]
       end
-
-      # Case: Noun. Example: "das Abonnement, -s"
-      if word.match('^(der|die|das) ')
-        word=arrayFirstColumn[0].split[1].strip
-        term.type=Word::TYPE[:noun]
-      end
-      
-      # Verb with sich. Example: Example: "amüsieren sich, amüsiert sich, amüsierte sich, hat sich amüsiert"
-      if word.match(' sich$')
-        word=word.sub(' sich$','')
-        term.type=Word::TYPE[:verb]
-      end 
     end
-    term.wordSimplified=word
-    term.url="http://en.pons.eu/dict/search/results/?q=#{word}&l=dees&in=&lf=de"
+
+    word.wordSimplified = cleanWord 
+    word.url = "http://en.pons.eu/dict/search/results/?q=#{cleanWord}&l=dees&in=&lf=de"
   end
-  return term
+
+  word 
 end
 
 def searchWordInDictionary(word, browser)
@@ -46,46 +51,49 @@ def searchWordInDictionary(word, browser)
     maybeAHeadword = tablerow.td(class: 'source').strong(class: 'headword')
     if maybeAHeadword.exists? and maybeAHeadword.text.strip == word.wordSimplified
       maybeATarget = tablerow.td(class: 'target')
-      word.translation = word.translation.push(maybeATarget.text) if maybeATarget.exists? and not word.translation.include?(maybeATarget.text) and not maybeATarget.text.include?('LatAm')
+      word.translation = word.translation.push(maybeATarget.text) if maybeATarget.exists? and not word.translation.include?(maybeATarget.text) and not maybeATarget.text.include?(WORD_LATIN_AMERICA)
     end
   end
   word.type = browser.h2.element(tag_name: 'acronym').text if browser.h2s.length == 1 and word.type == Word::TYPE[:other] and Word::TYPE.has_value?(browser.h2.element(tag_name: 'acronym').text) 
 end
 
-def prepCSVRow( term )
-  return [term.wordNotSimplified, term.translation.join(','), term.type, term.sentences]
+def prepCSVRow(word)
+  [word.wordNotSimplified, word.translation.join(','), word.type, word.sentences]
 end
 
-def obtainResultFileName(firstTerm, typeWord = 'all', fileParams = {pathCSVTranslation: '/Users/mcberros/workspace/translate_german_words/', fileNameTranslation: 'GermanWordsTranslation', sufixFile: '.csv'})
+def obtainResultFileName(firstWord, typeWord = 'all', fileParams = {pathCSVTranslation: '/Users/mcberros/workspace/translate_german_words/', fileNameTranslation: 'GermanWordsTranslation', sufixFile: '.csv'})
   fileName = "#{fileParams[:pathCSVTranslation]}#{fileParams[:fileNameTranslation]}_#{firstWord.wordSimplified}_#{typeWord}#{fileParams[:sufixFile]}"
 end
 
 def saveWordsInFile(allWords, splitPerType = true, typeWord = 'all', fileParams = {})
   if not allWords.empty?
-    if Word::TYPE.has_value?(typeWord)
-      selectedWords = allWords.select { |word| word.type == typeWord}
-      writeWords(selectedWords, typeWord)
-    else if typeWord == 'all'
+    if typeWord == 'all'
       if splitPerType
-
-      ###########Mal
-        Word::TYPE.each_value { |type| saveWordsInFile(allWords, typeWord = type)}
+        Word::TYPE.each_value { |typeWord| saveWordsPerTypeInFile(allWords, typeWord) }
       else
-        selectedWords = allWords
-        writeWords(selectedWords)
+        writeWords(allWords)
       end
+    elsif Word::TYPE.has_value?(typeWord)
+      saveWordsPerTypeInFile(allWords, typeWord)
     end
   end
 end
 
-def writeWords(words, typeWord='all', fileParams = {})
-  firstWord = words[0]
-  resultFileName = obtainResultFileName(firstWord, typeWord)
-  CSV.open(nameFile, "wb", {:col_sep => ";"}) do |csv|
-    words.each do |term|
-      csv << prepCSVRow(term)  
-    end
+def saveWordsPerTypeInFile(allWords, typeWord)
+  if not allWords.empty?
+    selectedWords = allWords.select { |word| word.type == typeWord}
+    writeWords(selectedWords, typeWord) if not selectedWords.empty?
   end
+end
+
+def writeWords(words, typeWord='all', fileParams = {})
+    firstWord = words[0]
+    resultFileName = obtainResultFileName(firstWord, typeWord)
+    CSV.open(resultFileName, WRITE_ACCESS_FILE_MODE, {col_sep:  ';'}) do |csvFile|
+      words.each do |word|
+        csvFile << prepCSVRow(word)  
+      end
+    end
 end
 
 begin
@@ -97,7 +105,7 @@ begin
   # We obtain from csv file the information, that we will use to search in 
   # the dictionary
   CSV.foreach(pathCSV_Source, {skip_blanks: true}) do |rowCSV_Source|
-    wordToSearch = obtainWordToSearch(rowCSV_Source)
+    wordToSearch = transformLineFileIntoWord(rowCSV_Source)
     wordsFromCSV_Source.push(wordToSearch) if not wordToSearch.nil?
   end
   
@@ -118,13 +126,9 @@ ensure
   # Los tres campos están separados por ;
 
   #Creamos un fichero por cada tipo
-  saveWordsInFile(wordsFromCSV_Source, splitPerType: true)
+  saveWordsInFile(wordsFromCSV_Source, splitPerType = true)
 
   #Creamos un unico fichero con todas las palabras
-  saveWordsInFile(wordsFromCSV_Source, splitPerType: false)
+  saveWordsInFile(wordsFromCSV_Source, splitPerType = false)
    
 end
-
-
-
-
