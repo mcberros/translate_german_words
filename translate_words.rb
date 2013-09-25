6f4e9e01eef5d0438c669e4c08124dac4776c222
mcberros <mcberros@gmail.com>
2013-09-25 21:30:28 +0200

    Applied ruby style

--- translate_words.rb
+++ translate_words.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'watir-webdriver'
 require 'csv'
 require_relative 'word'
@@ -6,149 +8,161 @@ Selenium::WebDriver::Firefox::Binary.path = '/Applications/Firefox.app/Contents/
 WORD_LATIN_AMERICA = 'LatAm'
 WRITE_ACCESS_FILE_MODE = 'wb'
 
-def read_words_from_file(pathCSV_Source)
-  wordsFromCSV_Source = []
+def read_words_from_file(path_csv_source)
+  words_from_csv_source = []
 
-  CSV.foreach(pathCSV_Source, { skip_blanks: true }) do |rowCSV_Source|
-    add_word_in_list(wordsFromCSV_Source, rowCSV_Source)
+  CSV.foreach(path_csv_source, { skip_blanks: true }) do |row_csv_source|
+    add_word_in_list(words_from_csv_source, row_csv_source)
   end
 
-  wordsFromCSV_Source
+  words_from_csv_source
 end
 
-def add_word_in_list(wordsFromCSV_Source, rowCSV_Source)
-  wordToSearch = transform_line_file_into_word(rowCSV_Source)
-  wordsFromCSV_Source.push(wordToSearch) if !wordToSearch.nil?
+def add_word_in_list(words_from_csv_source, row_csv_source)
+  word_to_search = transform_line_file_into_word(row_csv_source)
+  return if word_to_search.nil?
+  words_from_csv_source.push(word_to_search)
 end
 
-def transform_Line_File_Into_Word(rowCSV)
-  if valid_line_file?(rowCSV)
-    create_word(rowCSV)
-  end
+def transform_line_file_into_word(row_csv)
+  return unless valid_line_file?(row_csv)
+  create_word(row_csv)
 end
 
-def valid_line_file?(rowCSV) 
-  !rowCSV[0].nil? && !rowCSV[1].nil? && rowCSV[0].split(',').length >= 1
+def valid_line_file?(row_csv)
+  !row_csv.first.nil? && !row_csv.at(1).nil? && row_csv.first.split(',').length >= 1
 end
 
-def create_word(rowCSV)
+def create_word(row_csv) # HACK
   word = Word.new
-  word.wordNotSimplified = rowCSV[0]
-  word.sentences = rowCSV[1]
-  arrayWordNotSimplified = rowCSV[0].split(',')
-  word.wordSimplified = arrayWordNotSimplified[0].strip
-  if arrayWordNotSimplified.length > 1
-    if word.wordSimplified.match('^(der|die|das) ')
-      # Case: Noun. Example: "das Abonnement, -s"
-      word.wordSimplified = arrayWordNotSimplified[0].split[1].strip
-      word.type = Word::TYPE[:noun]
-    elsif word.wordSimplified.match(' sich$')
-      # Verb with sich. Example: Example: "amüsieren sich, amüsiert sich, amüsierte sich, hat sich amüsiert"
-      word.wordSimplified = word.wordSimplified.sub(' sich$','')
-      word.type = Word::TYPE[:verb]
-    else  
+  word.word_not_simplified = row_csv.first
+  word.sentences = row_csv.at(1)
+  array_word_not_simplified = row_csv.first.split(',')
+  word.word_simplified = array_word_not_simplified.first.strip
+
+  if is_word_a_noun_or_a_verb?(array_word_not_simplified)
+    if is_word_a_noun?(word)
+      word.word_simplified = array_word_not_simplified.first.split.at(1).strip
+      word.type = Word::TYPE.fetch(:noun)
+    elsif is_word_a_reflexive_verb?(word)
+      word.word_simplified = word.word_simplified.sub(' sich$', '')
+      word.type = Word::TYPE.fetch(:verb)
+    else # HACK
       # Verb without sich. Example: "abbiegen, biegt ab, bog ab, ist abgebogen"
-      word.type = Word::TYPE[:verb]
+      word.type = Word::TYPE.fetch(:verb)
     end
-  elsif arrayWordNotSimplified.length == 1
-    # Other word different from noun or verb: ab
-    word.type = Word::TYPE[:other]
+  elsif is_word_not_a_noun_or_not_a_verb?(array_word_not_simplified)
+    word.type = Word::TYPE.fetch(:other)
   end
-  word.url = "http://en.pons.eu/dict/search/results/?q=#{word.wordSimplified}&l=dees&in=&lf=de"
+
+  word.url = "http://en.pons.eu/dict/search/results/?q=#{word.word_simplified}&l=dees&in=&lf=de"
   word
 end
 
-def save_words_in_file(allWords, 
-                       splitPerType = true, 
-                       typeWord = 'all', 
-                       fileParams = { pathCSVTranslation: '/Users/mcberros/workspace/translate_german_words/', fileNameTranslation: 'GermanWordsTranslation', sufixFile: '.csv' })
-  if !allWords.empty?
-    if typeWord == 'all'
-      save_all_words(allWords, splitPerType, fileParams)
-    elsif Word::TYPE.has_value?(typeWord)
-      save_words_per_type_in_file(allWords, typeWord, fileParams)
+def is_word_not_a_noun_or_not_a_verb?(array_word_not_simplified)
+  array_word_not_simplified.size == 1
+end
+
+def is_word_a_noun_or_a_verb?(array_word_not_simplified)
+  array_word_not_simplified.length > 1
+end
+
+def is_word_a_noun?(word)
+  word.word_simplified.match('^(der|die|das) ')
+end
+
+def is_word_a_reflexive_verb?(word)
+  word.word_simplified.match(' sich$')
+end
+
+def save_words_in_file(all_words,
+                       split_per_type = true,
+                       type_word = 'all',
+                       file_params = { path_csv_translation: '/Users/mcberros/workspace/translate_german_words/', file_name_translation: 'GermanWordsTranslation', sufix_file: '.csv' })
+  return if all_words.empty?
+  if type_word == 'all'
+    save_all_words(all_words, split_per_type, file_params)
+  elsif Word::TYPE.value?(type_word)
+    save_words_per_type_in_file(all_words, type_word, file_params)
+  end
+end
+
+def save_all_words(all_words,
+                   split_per_type = true,
+                   file_params = { path_csv_translation: '/Users/mcberros/workspace/translate_german_words/', file_name_translation: 'GermanWordsTranslation', sufix_file: '.csv' })
+  return if all_words.empty?
+  if split_per_type
+    Word::TYPE.each_value do |type_word|
+      save_words_per_type_in_file(all_words, type_word)
     end
+  else
+    write_words(all_words, file_params)
   end
 end
 
-def save_all_words(allWords, 
-                   splitPerType = true, 
-                   fileParams = { pathCSVTranslation: '/Users/mcberros/workspace/translate_german_words/', fileNameTranslation: 'GermanWordsTranslation', sufixFile: '.csv' })
-  if !allWords.empty?
-    if splitPerType
-      Word::TYPE.each_value { |typeWord| save_words_per_type_in_file(allWords, typeWord) }
-    else
-      puts "caso splitPerType false"
-      write_words(allWords, fileParams)
-    end
+def save_words_per_type_in_file(all_words,
+                                type_word,
+                                file_params = { path_csv_translation: '/Users/mcberros/workspace/translate_german_words/', file_name_translation: 'GermanWordsTranslation', sufix_file: '.csv' })
+  return if all_words.empty?
+  selected_words = all_words.select { |word| word.type == type_word }
+  return if selected_words.empty?
+  write_words(selected_words, type_word, file_params)
+end
+
+def write_words(words,
+                type_word = 'all',
+                file_params = { path_csv_translation: '/Users/mcberros/workspace/translate_german_words/', file_name_translation: 'GermanWordsTranslation', sufix_file: '.csv' })
+  result_file_name = obtain_result_file_name(words, type_word, file_params)
+  write_words_in_file(words, result_file_name)
+end
+
+def obtain_result_file_name(words,
+                            type_word = 'all',
+                            file_params = { path_csv_translation: '/Users/mcberros/workspace/translate_german_words/', file_name_translation: 'GermanWordsTranslation', sufix_file: '.csv' })
+  "#{ file_params[:path_csv_translation] }#{ file_params[:file_name_translation] }_#{ words.first.word_simplified }_#{ type_word }#{ file_params[:sufix_file] }"
+end
+
+def write_words_in_file(words, result_file_name)
+  CSV.open(result_file_name, WRITE_ACCESS_FILE_MODE, { col_sep: ';' }) do |csv_file|
+    words.each { |word| csv_file << prepare_csv_row(word) }
   end
+  # CSV.close # Review
 end
 
-def save_words_per_type_in_file(allWords, 
-                                typeWord, 
-                                fileParams = { pathCSVTranslation: '/Users/mcberros/workspace/translate_german_words/', fileNameTranslation: 'GermanWordsTranslation', sufixFile: '.csv' })
-  if !allWords.empty?
-    selectedWords = allWords.select { |word| word.type == typeWord }
-    write_words(selectedWords, typeWord, fileParams) if !selectedWords.empty?
-  end
+def prepare_csv_row(word)
+  [word.word_not_simplified, word.translation.join(','), word.type, word.sentences]
 end
 
-def write_words(words, 
-                typeWord = 'all', 
-                fileParams = { pathCSVTranslation: '/Users/mcberros/workspace/translate_german_words/', fileNameTranslation: 'GermanWordsTranslation', sufixFile: '.csv' }) 
-  resultFileName = obtain_result_file_name(words, typeWord, fileParams)
-  puts "#{resultFileName}"
-  write_words_in_file(words, resultFileName)
-end
-
-def obtain_result_file_name(words, 
-                            typeWord = 'all', 
-                            fileParams = { pathCSVTranslation: '/Users/mcberros/workspace/translate_german_words/', fileNameTranslation: 'GermanWordsTranslation', sufixFile: '.csv' })
-  firstWord = words[0]
-  "#{fileParams[:pathCSVTranslation]}#{fileParams[:fileNameTranslation]}_#{firstWord.wordSimplified}_#{typeWord}#{fileParams[:sufixFile]}"
-end
-
-def write_words_in_file(words, resultFileName)
-  CSV.open(resultFileName, WRITE_ACCESS_FILE_MODE, { col_sep:  ';' }) do |csvFile|
-    words.each do |word|
-      csvFile << prepare_CSV_row(word)
-    end
-  end
-end
-
-def prepare_CSV_row(word)
-  [word.wordNotSimplified, word.translation.join(','), word.type, word.sentences]
-end
-
-def search_all_words(wordsFromCSV_Source, browser)
-  wordsFromCSV_Source.each do |word|
+def search_all_words(words_from_csv_source, browser)
+  words_from_csv_source.each do |word|
     word.search_word_in_dictionary(browser)
-    puts "#{word.wordSimplified}"
+    puts "#{ word.to_s }"
   end
 end
 
 begin
   browser = Watir::Browser.new :firefox
-  # Path where is the file with the list of words.
-  pathCSV_Source = '/Users/mcberros/workspace/translate_german_words/GermanWords.csv'
-    
-  wordsFromCSV_Source = read_words_from_file(pathCSV_Source)
-  search_all_words(wordsFromCSV_Source, browser)
+  path_csv_file_words_input = '/Users/mcberros/workspace/translate_german_words/GermanWords.csv'
+
+  words_from_csv_source = read_words_from_file(path_csv_file_words_input)
+  search_all_words(words_from_csv_source, browser)
 
 ensure
   browser.close
 
   # Para cada palabra vamos a grabar un csv con tres columnas:
-  # 1. Palabra original del csv, es decir, no simplificada
-  # 2. Traducción de la palabra simplificada al español
-  # 3. Tipo de palabra
-  # 4. Frases de ejemplo
-  # Los tres campos están separados por ;
+  #   1. Palabra original del csv, es decir, no simplificada.
+  #   2. Traducción de la palabra simplificada al español.
+  #   3. Tipo de palabra.
+  #   4. Frases de ejemplo.
+  #   Los tres campos están separados por ';'.
 
-  #Creamos un fichero por cada tipo
-  save_words_in_file(wordsFromCSV_Source, splitPerType = true) if !wordsFromCSV_Source.nil?
+  # Creamos un fichero por cada tipo.
+  # HACK
+  save_words_in_file(words_from_csv_source, split_per_type = true) unless words_from_csv_source.nil?
 
-  #Creamos un unico fichero con todas las palabras
-  #saveWordsInFile(wordsFromCSV_Source, splitPerType = false)
+  # Creamos un unico fichero con todas las palabras.
+  # HACK
+  # saveWordsInFile(wordsFromCSV_Source, split_per_type = false)
    
 end
--- word.rb
+++ word.rb
@@ -1,65 +1,68 @@
-class Word
-  
-  TYPE = {noun: 'NOUN', 
-          verb: 'VERB',
-          adverb: 'ADV',
-          adjective: 'ADJ',
-          conjuntion: 'CONJ',
-          preposition: 'PREP',
-          other: 'OTHER'}
+# encoding: UTF-8
 
-  attr_accessor :wordNotSimplified, :wordSimplified, :url, :sentences, :translation, :type
-  
-  def initialize()
+class Word
+
+  TYPE = { noun: 'NOUN',
+           verb: 'VERB',
+           adverb: 'ADV',
+           adjective: 'ADJ',
+           conjuntion: 'CONJ',
+           preposition: 'PREP',
+           other: 'OTHER' }
+
+  attr_accessor :word_not_simplified, :word_simplified, :url, :sentences, :translation, :type
+
+  def initialize
     @translation = []
   end
 
-  def searchWordInDictionary(browser)
+  def search_word_in_dictionary(browser)
     browser.goto @url
-    explorePageForWord(browser)
+    explore_page_for_word(browser)
   end
 
-  def explorePageForWord(browser)
-    translationTable = browser.trs(class: 'kne')
-    obtainTranslationListForWord(translationTable)
-    exploreTypeOfWord(browser)
+  def explore_page_for_word(browser)
+    translation_table = browser.trs(class: 'kne')
+    obtain_translation_list_for_word(translation_table)
+    explore_type_of_word(browser)
   end
-  
-  def obtainTranslationListForWord(translationTable)
-    translationTable.each do |tableRow|
-      exploreTableRowForTranslation(tableRow)
+
+  def obtain_translation_list_for_word(translation_table)
+    translation_table.each do |table_row|
+      explore_table_row_for_translation(table_row)
     end
   end
 
-  def exploreTypeOfWord(browser)
-    if changeWordType?(browser)
-      @type = browser.h2.element(tag_name: 'acronym').text
-    end
-  end
-  
-  def changeWordType?(browser)
-    browser.h2s.length == 1 and @type == Word::TYPE[:other] and Word::TYPE.has_value?(browser.h2.element(tag_name: 'acronym').text)
+  def explore_type_of_word(browser)
+    @type = browser.h2.element(tag_name: 'acronym').text if change_word_type?(browser)
   end
 
-  def exploreTableRowForTranslation(tableRow)
-    maybeAHeadword = tableRow.td(class: 'source').strong(class: 'headword')
-    if maybeATarget?(maybeAHeadword)
-      maybeATarget = tableRow.td(class: 'target')
-      pushNewTranslationForWord(maybeATarget)
-    end
+  def change_word_type?(browser)
+    browser.h2s.size == 1 && @type == Word::TYPE[:other] && Word::TYPE.value?(browser.h2.element(tag_name: 'acronym').text)
   end
 
-  def maybeATarget?(maybeAHeadword)
-    maybeAHeadword.exists? and maybeAHeadword.text.strip == @wordSimplified
+  def explore_table_row_for_translation(table_row)
+    maybe_a_headword = table_row.td(class: 'source').strong(class: 'headword')
+    return unless maybe_a_target?(maybe_a_headword)
+    maybe_a_target = table_row.td(class: 'target')
+    push_new_translation_for_word(maybe_a_target)
   end
 
-  def pushNewTranslationForWord(maybeATarget)
-    if isATranslation?(maybeATarget)
-      @translation = @translation.push(maybeATarget.text)
-    end
+  def maybe_a_target?(maybe_a_headword)
+    maybe_a_headword.exists? && maybe_a_headword.text.strip == @word_simplified
   end
 
-  def isATranslation?(maybeATarget)
-    maybeATarget.exists? and not @translation.include?(maybeATarget.text) and not maybeATarget.text.include?(WORD_LATIN_AMERICA)
+  def push_new_translation_for_word(maybe_a_target)
+    return unless is_a_translation?(maybe_a_target)
+    @translation = @translation.push(maybe_a_target.text)
   end
+
+  def is_a_translation?(maybe_a_target)
+    maybe_a_target.exists? && !@translation.include?(maybe_a_target.text) && !maybe_a_target.text.include?(WORD_LATIN_AMERICA)
+  end
+
+  def to_s
+    "#{ @word_simplified }"
+  end
+
 end
